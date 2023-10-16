100cd7635bc3db8124678be8ea3b6287bc0a9a2d
kubaporebski <kubaporebski>
2023-10-16 12:03:29 +0000

    Another parts of the homework

--- notebooks/StreamApp.py
+++ notebooks/StreamApp.py
@@ -2,7 +2,7 @@
 # MAGIC %md
 # MAGIC # Spark Streaming homework
 # MAGIC
-# MAGIC ## TODO:
+# MAGIC ## Goals of the task:
 # MAGIC * Create Spark Structured Streaming application with Auto Loader to incrementally and efficiently processes hotel/weather data as it arrives in provisioned Storage Account. Using Spark calculate in Databricks Notebooks for each city each day:
 # MAGIC   * Number of distinct hotels in the city.
 # MAGIC   * Average/max/min temperature in the city.
@@ -17,14 +17,15 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,Prepare the current notebook's environment
+import time 
+import threading
+
 # remove old files, if they are there
 dbutils.fs.rm("dbfs:/tmp/checkpoints", True)
 dbutils.fs.rm("dbfs:/user/hive/warehouse/hotel_weather", True)
 
-# for limited time of streaming
-import time 
-import threading
-
+# method for stopping a Spark's StreamingQuery after a given number of seconds
 def wait_and_stop(query, time_limit):
     print(f"Running for {time_limit} seconds... ")
     time.sleep(time_limit)
@@ -33,6 +34,7 @@ def wait_and_stop(query, time_limit):
 
 # COMMAND ----------
 
+# DBTITLE 1,Create a Spark Stream
 # create a stream
 hw_stream = spark.readStream \
     .format("cloudFiles") \
@@ -68,7 +70,7 @@ hw_stream = spark.readStream \
 
 # COMMAND ----------
 
-# DBTITLE 1,Start a streaming
+# DBTITLE 1,Start the streaming operation
 # how much time we will be running this (current cell) streaming code? [seconds]
 time_limit = 120 
 
@@ -84,6 +86,7 @@ thr.start()
 
 # COMMAND ----------
 
+# DBTITLE 1,Check how many records are there in a DBFS table
 # MAGIC %sql
 # MAGIC -- check how many records are there now
 # MAGIC select concat('There are ', count(*), ' rows currently.') as total_records_number_info from hotel_weather
@@ -112,11 +115,67 @@ thr.start()
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC select wthr_date, city, 
-# MAGIC   round(min(avg_tmpr_c), 2) as min_temperature_c,
-# MAGIC   round(avg(avg_tmpr_c), 2) as mean_temperature_c,
-# MAGIC   round(max(avg_tmpr_c), 2) as max_temperature_c,
-# MAGIC   count(*) as records
-# MAGIC from hotel_weather
-# MAGIC group by wthr_date, city
-# MAGIC order by city
+# MAGIC create or replace table hotel_weather_stats as 
+# MAGIC   select wthr_date, city, 
+# MAGIC     round(min(avg_tmpr_c), 2) as min_temperature_c,
+# MAGIC     round(avg(avg_tmpr_c), 2) as mean_temperature_c,
+# MAGIC     round(max(avg_tmpr_c), 2) as max_temperature_c,
+# MAGIC     count(*) as records
+# MAGIC   from hotel_weather
+# MAGIC   group by wthr_date, city
+# MAGIC   order by city, wthr_date
+# MAGIC ;
+# MAGIC
+# MAGIC select * from hotel_weather_stats
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Visualize incoming data in Databricks Notebook for 10 biggest cities (the biggest number of hotels in the city, one chart for one city):
+# MAGIC * X-axis: date (date of observation).
+# MAGIC * Y-axis: number of distinct hotels, average/max/min temperature.
+
+# COMMAND ----------
+
+# DBTITLE 1,Finding top 10 biggest cities, to store them in a temp view
+# MAGIC %sql
+# MAGIC create or replace temp view top10_cities as
+# MAGIC   with cte (
+# MAGIC     select 
+# MAGIC       city, 
+# MAGIC       count(distinct id) as hotels_in_city, 
+# MAGIC       row_number() over (order by count(distinct id) desc) as rank_number
+# MAGIC     from hotel_weather
+# MAGIC     group by city
+# MAGIC   )
+# MAGIC   select * from cte where rank_number <= 10
+# MAGIC ;
+# MAGIC
+# MAGIC select * from top10_cities
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create or replace temp view top10_cities_with_stats as (
+# MAGIC   select 
+# MAGIC     top.city, 
+# MAGIC     round(avg(stats.min_temperature_c), 2) as avg_min_temperature_c,
+# MAGIC     round(avg(stats.mean_temperature_c), 2) as avg_mean_temperature_c,
+# MAGIC     round(avg(stats.max_temperature_c), 2) as avg_max_temperature_c,
+# MAGIC     max(top.hotels_in_city) as hotels_in_city
+# MAGIC   from top10_cities as top
+# MAGIC       join hotel_weather_stats as stats on top.city=stats.city
+# MAGIC   group by top.city
+# MAGIC )
+# MAGIC ;
+# MAGIC
+# MAGIC select * from top10_cities_with_stats
+
+# COMMAND ----------
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+
